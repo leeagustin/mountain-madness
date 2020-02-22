329c72a5b8c6a63029779025ed76dd6bf29d7901
rnjesus <tjengbryan@gmail.com>
2020-02-22 14:52:19 -0800

    using phaser.io

---
+++ public/scripts/main.js
@@ -0,0 +1,20 @@
+var config={
+    type: Phaser.AUTO,
+    width: 100%,
+    height: 100%,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //debug: true,
+            gravity: { y: 0 }
+
+        }
+    },
+    scene:{
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
