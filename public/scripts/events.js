74aa3f7dd9762049403174026797c2cec2d93fc7
leeagustin <leeagustin@hotmail.com>
2020-02-23 01:00:50 -0800

    renamed function

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -15,7 +15,7 @@ function initializeRandomEventGenerator() {
 function foodSpoilage() {
     if (Math.random() < 0.03 && !isFoodSpoiling) {
         foodRateModifier += 1;
-        showEventText('Your food is spoiling!');
+        displayAnnouncement('Event: Your food is spoiling!');
         isFoodSpoiling = true;
     }
 }
@@ -23,7 +23,7 @@ function foodSpoilage() {
 function waterLeakage() {
     if (Math.random() < 0.03 && !isWaterLeaking) {
         waterRateModifier += 1;
-        showEventText('Your water storage has suffered a leakage!');
+        displayAnnouncement('Event: Your water storage has suffered a leakage!');
         isWaterLeaking = true;
     }
 }
@@ -31,30 +31,21 @@ function waterLeakage() {
 function asteroidHitWaterSupply() {
     if (Math.random() < 0.03) {
         waterLevel -= 1000;
-        showEventText('An asteroid has hit your water supply!');
+        displayAnnouncement('Event: An asteroid has hit your water supply!');
     }
 }
 
 function ratsAteFoodSupply() {
     if (Math.random() < 0.03) {
         foodLevel -= 1000;
-        showEventText('Rats ate some of the food supply!');
+        displayAnnouncement('Event: Rats ate some of the food supply!');
     }
 }
 
 function createEngineer() {
     setTimeout(() => {
         engineers += 1;
-        showEventText('An engineer has been created!');
+        displayAnnouncement('Event: An engineer has been created!');
         createEngineer();
     }, 30000);
-}
-
-function showEventText(msg) {
-    eventsTextBox.setText(`Event: ${msg}`);
-    oneUpSound.loop = false;
-    oneUpSound.play();
-    setTimeout(() => {
-        eventsTextBox.setText('');
-    }, 3000);
 }
