77fad2c0c893adebc6a01e61b769fb06d0747edd
andiz <andiz@sfu.ca>
2020-02-22 23:56:20 -0800

    beautified

--- public/scripts/actions.js
+++ public/scripts/actions.js
@@ -8,84 +8,84 @@ let harvestingMetal = false;
 
 
 function harvestFood() {
-  if (harvestingFood)
-    return;
-  harvestingFood = true;
-  moveFarmProgressBar();
-  setTimeout(() => {
-    foodLevel += foodHarvestingRate;
-    harvestingFood = false;
-  }, 10000);
+    if (harvestingFood)
+        return;
+    harvestingFood = true;
+    moveFarmProgressBar();
+    setTimeout(() => {
+        foodLevel += foodHarvestingRate;
+        harvestingFood = false;
+    }, 10000);
 }
 
 function harvestWater() {
-  if (harvestingWater)
-    return;
-  harvestingWater = true;
-  moveWaterProgressBar();
-  setTimeout(() => {
-    waterLevel += waterHarvestingRate;
-    harvestingWater = false;
-  }, 13000);
+    if (harvestingWater)
+        return;
+    harvestingWater = true;
+    moveWaterProgressBar();
+    setTimeout(() => {
+        waterLevel += waterHarvestingRate;
+        harvestingWater = false;
+    }, 13000);
 }
 
 function harvestMetal() {
-  if (harvestingMetal)
-    return;
-  harvestingMetal = true;
-  moveMetalProgressBar();
-  setTimeout(() => {
-    metalLevel += metalHarvestingRate;
-    harvestingMetal = false;
-  }, 15000);
+    if (harvestingMetal)
+        return;
+    harvestingMetal = true;
+    moveMetalProgressBar();
+    setTimeout(() => {
+        metalLevel += metalHarvestingRate;
+        harvestingMetal = false;
+    }, 15000);
 
 }
 
 
 function moveFarmProgressBar() {
-  var elem = document.getElementById("farmBar");
-  var width = 1;
-  var id = setInterval(frame, 100);
+    var elem = document.getElementById("farmBar");
+    var width = 1;
+    var id = setInterval(frame, 100);
 
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      elem.style.width = 0;
-    } else {
-      width++;
-      elem.style.width = width + "%";
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+            elem.style.width = 0;
+        } else {
+            width++;
+            elem.style.width = width + "%";
+        }
     }
-}
 
-function moveMetalProgressBar() {
-  var elem = document.getElementById("metalBar");
-  var width = 1;
-  var id = setInterval(frame, 150);
+    function moveMetalProgressBar() {
+        var elem = document.getElementById("metalBar");
+        var width = 1;
+        var id = setInterval(frame, 150);
 
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      elem.style.width = 0;
-    } else {
-      width++;
-      elem.style.width = width + "%";
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                elem.style.width = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
     }
-  }
-}
 
-function moveWaterProgressBar() {
+    function moveWaterProgressBar() {
 
-  var elem = document.getElementById("waterBar");
-  var width = 1;
-  var id = setInterval(frame, 240);
+        var elem = document.getElementById("waterBar");
+        var width = 1;
+        var id = setInterval(frame, 240);
 
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      elem.style.width = 0;
-    } else {
-      width++;
-      elem.style.width = width + "%";
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                elem.style.width = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
     }
-  }
-}
